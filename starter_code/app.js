63e64e323913fda8217783dae48d2662389c2444
sogchs <sogchs@gmail.com>
2018-12-27 11:41:27 +0100

    Done

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,10 +5,12 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 
@@ -16,6 +18,31 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers', {beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    console.log(beers);
+    res.render('random-beers',{beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 
-app.listen(3000);
+
+
+
+app.listen(PORT, () => {
+  console.info(`App listen at ${PORT} port`);
+});
